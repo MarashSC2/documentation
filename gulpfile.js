cef98fe1e722b75da009c13e4ce89e5d297dbdaa
MarashSC2 <marashsc@freenet.de>
2015-09-08 20:59:36 +0200

    script added, gulpfile modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,41 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	watch = require('gulp-watch'),
-	batch = require('gulp-batch');
+	batch = require('gulp-batch'),
+	sass = require('gulp-sass'),
+	rename = require("gulp-rename");;
 	
 var paths = {
 	indexSrc: [	
 		'./src/index.top.html',
 		'./src/components/menu.html',
+		'./src/components/test.html',
 		'./src/index.bottom.html'
 	],
-	watch: './src/**/*'
+	watch: './src/**/*',
+	dst: './build',
+	sassSrc: './src/style.scss',
+	scriptSrc: './src/script.js'
 };
+
+gulp.task('script', function(){
+	return gulp.src(paths.scriptSrc)
+	.pipe(watch(paths.scriptSrc))
+    .pipe(gulp.dest(paths.dst));
+})
 
 gulp.task('index', function() {
   return gulp.src(paths.indexSrc)
     .pipe(concat('index.html'))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('style',function(){
+	return gulp.src(paths.sassSrc)
+	.pipe(watch(paths.sassSrc))
+	.pipe(rename('style.cs'))
+	.pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.dst));
 });
 
 gulp.task('watch', function () {
@@ -24,4 +44,4 @@ gulp.task('watch', function () {
     }));
 });
 
-gulp.task('default', ['index','watch']);
+gulp.task('default', ['index','style','script','watch']);
