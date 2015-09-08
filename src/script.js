e2e546588ad7e790f52f8d76bd170bafa402d7a5
MarashSC2 <marashsc@freenet.de>
2015-09-08 21:14:50 +0200

    jquery menu

--- src/script.js
+++ src/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $("#menu").click(function(){
-        $("#menuContent").toggle();
+    $("#menu").hover(function(){
+        $("#menuContent").show();
+    },function(){
+        $("#menuContent").hide();
+    });
+    $("#menu").hover(function(){
+        $(this).fadeTo(250,1);
+    },function(){
+        $(this).fadeTo(250,0.5);
     });
 });
